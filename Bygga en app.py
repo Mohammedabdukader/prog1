7a09de2e57497d01388081d05985338a7831f1e5
mohammed <mohammad.abdukader@elev.ga.ntig.se>
2022-09-29 13:27:01 +0200

    ss

---
+++ Bygga en app.py
@@ -0,0 +1,17 @@
+def test(name = input("Skriv namn: ")):
+    print(name, "is best")
+
+test()
+
+def prov (namn = input("skriv din namn: ")):
+    print (namn, "är den bästa namnet")
+
+prov()
+
+while True
+nam = input ("var föddes Arda?: "):
+if nam== "Sverige"
+    print ("helt rätt")
+
+else:
+    print ("Tyvärr fel")
