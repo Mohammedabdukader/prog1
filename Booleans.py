93ab10cc99c809b072a494ce33626793466424a5
mohammed <mohammad.abdukader@elev.ga.ntig.se>
2022-10-10 13:21:26 +0200

    quiz fixat

---
+++ Booleans.py
@@ -0,0 +1,24 @@
+#Booleans
+print (5==5)
+
+#if else elif 
+name = input ("Name?").capitalize()
+
+#if else i variabeldeklaration 
+
+#time.sleep(1)
+
+#match case
+match name:
+    case "Orvar":
+        print ("hej orvar")
+
+for i in range (1,60):
+    print (i)
+
+if i % 3==0:
+    print ("fizz")
+elif i % 5==0 and i % 3==0:
+    print ("fizzbuzz")
+else:
+    print (i)
--- mm.py
+++ mm.py
@@ -1,31 +1,31 @@
-score =0
-fot = input ("vilket lag hejar Mohammed?").capitalize
-if fot == ("real madrid"):
+score = 0
+fot = input ("vilket lag hejar Mohammed?").capitalize()
+if fot == ("Real madrid"):
     print ("rätt som fan")
     score +=3
 else: 
     print ("tyvärr fel")
 
-spelare = input ("vem är världens bästa spelare enligt mohammed?").capitalize
+spelare = input ("vem är världens bästa spelare enligt mohammed?").capitalize()
 if spelare == ("Benzema"):
     score +=3
     print ("korrekt")
 else: 
     print ("tyvärr fel")
 
-Elit = input("Hur många Ballon d'Or har ronaldo vunnit?").capitalize
+Elit = input("Hur många Ballon d'Or har ronaldo vunnit?").capitalize()
 if Elit == ("5"):
     print ("korrekt")
     score +=3
 else: print ("tyvärr fel")
 
-mål = input("hur många mål har Benzema gjort under 2021/2022 säsongen?")
+mål = input("hur många mål har Benzema gjort under 2021/2022 säsongen?").capitalize()
 if mål == ("100 mål"):
     print ("helt rätt")
     score +=3
 
 else: print ("tyvärr fel")
 
-
+print (f"du har {score} poäng")
    
 
